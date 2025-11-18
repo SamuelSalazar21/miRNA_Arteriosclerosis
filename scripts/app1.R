87aad85af0220a250c80f186f758fe325ced00d1
Frank-Melo <frankyo72@gmail.com>
2025-11-18 17:38:48 -0500

    Update app1.R

--- scripts/app1.R
+++ scripts/app1.R
@@ -1,3 +1,5 @@
+# Programa basico de Shiny
+
 getwd()
 setwd("C:/Users/PC/Documents")
 
@@ -7,3 +9,4 @@ library(quantmod)
 runApp("stockVis")
 runGist("eb3470beb1c0252bd0289cbc89bcf36f")
 install.packages("ggplot2")
+
